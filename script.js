aad7d6f357a105f03892a013d9a3ba5553a015bb
ACHilmy <achmad.hilmy@outlook.com>
2025-02-25 21:34:40 +0700

    v0.2: + Show Result and The Winner.

--- script.js
+++ script.js
@@ -1,14 +1,55 @@
-function diceValue() {
+const DICE_FACE_FILE_NAME = "dice";
+const DICE_FACE_FILE_EXTENSION = ".png";
+
+function dieRandomValue() {
     return Math.ceil(Math.random() * 6);
 }
 
-function rollDice(element, imageFolder) {
-    const diceAnimation = setInterval(function() {
-            let diceNumber = diceValue();
-            element.setAttribute("src", imageFolder + "dice" + diceNumber + ".png");
-        },
-        100
-    );
+function dieFaceSwap(die, imageFolder) {
+    die.setAttribute("src", imageFolder + DICE_FACE_FILE_NAME + dieRandomValue() + DICE_FACE_FILE_EXTENSION);
+}
 
-    setTimeout(function() {clearInterval(diceAnimation)}, 3000);
+function diceRoll(dice, imageFolder, labelElement) {
+    let diceValue=[];
+    let diceAnimation = setInterval(() => dice.forEach(die => dieFaceSwap(die, imageFolder)), 100);
+    let maxValue=0;
+    let result="";
+
+    labelElement.innerHTML = "Rolling...";
+
+    setTimeout(
+        function () {
+            clearInterval(diceAnimation);
+            dice.forEach(die => {
+                let fileName = die.getAttribute("src");
+                diceValue.push(fileName.slice(fileName.lastIndexOf(".")-1, fileName.lastIndexOf(".")));
+            });
+
+            for(let i=0; i < diceValue.length; i++) {
+                if (diceValue[i] > diceValue[i-1]){
+                    result = "win";
+                    maxValue = i;
+                }
+                else if(diceValue[i] === diceValue[i-1]) {
+                    result = "draw";
+                }
+                else {
+                    result = "win";
+                    maxValue = i-1;
+                }
+            }
+
+            showResult(maxValue, result, labelElement);
+        }
+        , 3000
+    );
+}
+
+function showResult(dieNumber, result, labelElement) {
+    if (result === "draw") {
+        labelElement.innerHTML = "DRAW";
+    }
+    else {
+        labelElement.innerHTML = "Player " + (dieNumber+1) + " WINS!";
+    }
 }
